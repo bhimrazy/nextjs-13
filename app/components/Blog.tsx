af8a038ef95ba15d049cce1a52c0739e1a71aba5
BHIMRAJ YADAV <bhimrajyadav977@gmail.com>
2022-11-03 02:00:10 +0000

    updated

--- app/components/Blog.tsx
+++ app/components/Blog.tsx
@@ -1,49 +1,38 @@
 import Image from "next/image";
-async function getData() {
-  const res = await fetch(
-    "https://3000-bhimrazy-nextjs13-azeedfo3fe9.ws-us73.gitpod.io/api/"
-  );
 
-  return res.json();
-}
-
-const BlogDetails = async () => {
-  const { data } = await getData();
-  return (
-    <ul className="flex flex-col gap-6">
-      {data &&
-        data.map((blog: any, i: any) => (
-          <li key={i}>
-            <article
-              key={i}
-              className="grid cursor-pointer grid-cols-1 space-y-4 rounded-md bg-white p-6 shadow-sm transition hover:shadow-md lg:grid-cols-3"
-            >
-              <div className="relative h-40 w-40 overflow-hidden rounded-md bg-slate-200">
-                <Image
-                  src="https://bhimraj.com.np/bhimraj_yadav.jpg"
-                  alt={blog?.title}
-                  className=" object-contain "
-                  fill
-                  // width={500}
-                  // height={500}
-                  blurDataURL={blog?.thumbnail}
-                  placeholder="blur"
-                />
-              </div>
-              <div className="space-y-2 lg:col-span-2">
-                <h1 className="text-xl font-bold">{blog?.title}</h1>
-                <p className="text-normal text-gray-600">{blog?.description}</p>
-              </div>
-            </article>
-          </li>
-        ))}
-    </ul>
-  );
-};
-export default function Blog() {
+export default function Blog({ data }: any) {
   return (
     <div className="flex max-w-3xl flex-col place-content-center">
-      <BlogDetails />
+      <ul className="flex flex-col gap-6">
+        {data &&
+          data.map((blog: any, i: any) => (
+            <li key={i}>
+              <article
+                key={i}
+                className="grid cursor-pointer grid-cols-1 space-y-4 rounded-md bg-white p-6 shadow-sm transition hover:shadow-md lg:grid-cols-3"
+              >
+                <div className="relative h-40 w-40 overflow-hidden rounded-md bg-slate-200">
+                  <Image
+                    src="https://bhimraj.com.np/bhimraj_yadav.jpg"
+                    alt={blog?.title}
+                    className=" object-contain "
+                    fill
+                    // width={500}
+                    // height={500}
+                    blurDataURL={blog?.thumbnail}
+                    placeholder="blur"
+                  />
+                </div>
+                <div className="space-y-2 lg:col-span-2">
+                  <h1 className="text-xl font-bold">{blog?.title}</h1>
+                  <p className="text-normal text-gray-600">
+                    {blog?.description}
+                  </p>
+                </div>
+              </article>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,7 +1,34 @@
 import Blog from "./Blog";
 import Counter from "./Counter";
+async function getData() {
+  // const res = await fetch(
+  //   "https://3000-bhimrazy-nextjs13-azeedfo3fe9.ws-us73.gitpod.io/api/"
+  // );
+  // return res.json();
+  await new Promise((res) => setTimeout(res, 200));
+
+  // You would usually return data from an API here.
+  // return res.json();
+  const blog_content = [
+    {
+      title: "Surface Pro 8 – The most powerful Pro - Microsoft",
+      thumbnail: "https://images.unsplash.com/photo-1583418855863-858dc79c2d19",
+      description:
+        "Surface Pro 8 is designed to light up the best of Windows 11, enabling you to work, play, and create in ways most natural to you.",
+    },
+    {
+      title: "The M2 MacBook Air Is the Ultimate Laptop Gift",
+      thumbnail:
+        "https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/photo_1517336714731_489689fd1ca8_9.jpeg",
+      description:
+        "With a new camera, new colors and a bigger screen, the MacBook Air remains one of the most universally useful laptops you can get.",
+    },
+  ];
+  return blog_content;
+}
 
 export default async function Components() {
+  const data = await getData();
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
       <main className="flex min-h-screen flex-col place-content-center space-y-6">
@@ -11,7 +38,7 @@ export default async function Components() {
           </h2>
           <Counter />
           {/* <Suspense fallback={<p>Loading feed...</p>}> */}
-          <Blog />
+          <Blog data={data} />
           {/* </Suspense> */}
         </section>
       </main>
